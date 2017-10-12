767d45c485a7501fc07b36dd9aee8ef40e5e41c5
Shaojung Lu <shaojung@gmail.com>
2017-10-12 13:40:24 +0800

    convert JSON by gson

---
+++ app/src/main/java/com/example/teacher/t2017101201/AnimalHome.java
@@ -0,0 +1,12 @@
+package com.example.teacher.t2017101201;
+
+/**
+ * Created by teacher on 2017/10/12.
+ */
+
+public class AnimalHome {
+    public String district;
+    public String address;
+    public String tel;
+    public String opening_hours;
+}
